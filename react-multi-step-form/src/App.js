eabcace00b0818056e99758196d3c47901dbd7e0
tlc2880 <tommylcao@gmail.com>
2023-09-25 11:47:47 -0700

    Correct last branch push error

--- react-multi-step-form/src/App.js
+++ react-multi-step-form/src/App.js
@@ -5,6 +5,8 @@ function App() {
 
   return (
     <FormProvider>
+      <h2 className='center'>Multi-Step Form</h2>
+      <h2 className='center'>Version 2.0 using Context API</h2>
       <Form />
     </FormProvider>
   )
